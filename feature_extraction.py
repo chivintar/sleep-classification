61bf49bf5b2ab426a3a29bf077e66ca378efa542
Chivintar Amenty <chivintar@ntropy.eu>
2025-05-31 20:30:08 +0300

    Version 1 - working ipynb

--- feature_extraction.py
+++ feature_extraction.py
@@ -6,8 +6,11 @@ from scipy.signal import welch
 print('Running feature_extraction.py')
 
 def extract_time_features(epoch_data):
-    # epoch_data shape: (n_channels, n_times)
-    ''' Computes the first four statistical moments of each epoch, together with the root mean square and the peak-to-peak amplitude. Returns a flat list of the features of each channel .
+    ''' 
+        Takes epochs object (shape: (number_of_channels, number_of_epochs) ).
+        Computes the first four statistical moments of each epoch, 
+        together with the root mean square and the peak-to-peak amplitude. 
+        Returns a flat list of the features of each channel for individual epoch (shape: number_of_channels, 7).
     '''
     features = []
     for ch_data in epoch_data:
@@ -17,7 +20,7 @@ def extract_time_features(epoch_data):
             skew(ch_data),
             kurtosis(ch_data),
             np.sqrt(np.mean(ch_data**2)),  # RMS
-            np.ptp(ch_data),               # Peak-to-peak
+            np.ptp(ch_data),               # Peak to peak
             ((ch_data[:-1] * ch_data[1:]) < 0).sum()  # Zero crossings
         ])
     return features
@@ -25,9 +28,13 @@ def extract_time_features(epoch_data):
 
 
 def bandpower(data, sf, band, window_seconds=4):
-    ''' Returns a scalar that depicts the average band power in the given frequency range (Delta, Theta, Alpha, Beta) with the Welch method, a spectral density estimation method. https://en.wikipedia.org/wiki/Welch%27s_method, and for the scipy function: https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.welch.html#welch
+    ''' 
+    Returns a scalar that depicts the average band power in the given frequency range (Delta, Theta, Alpha, Beta)
+    with the Welch method, a spectral density estimation method. https://en.wikipedia.org/wiki/Welch%27s_method, 
+    using the build-in scipy function: https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.welch.html#welch
 
-    The widnow_seconds are set to 4 (typical for EEG) so that for 100Hz sampling frequency we have 400samples for each segment. More seconds lead to finer frequency-bins and fewwer seconds lead to more stable segments. Adapted from Raphael Vallat
+    The widnow_seconds are set to 4 (typical for EEG) so that for 100Hz sampling frequency we have 400samples for each segment. 
+    More seconds lead to finer frequency-bins and fewwer seconds lead to more stable segments. Function adapted from Raphael Vallat.
     '''
     band = np.asarray(band)
     freqs, psd = welch(data, sf, nperseg=window_seconds * sf) # default with Hann tapering and 50% overlapping
@@ -35,8 +42,13 @@ def bandpower(data, sf, band, window_seconds=4):
     idx_band = np.logical_and(freqs >= band[0], freqs <= band[1]) #this is to select only the freq bins inside the desired band
     return psd[idx_band].sum() * freq_res #the approximate integration
 
+
+
+
 def extract_frequency_features(epoch_data, sf):
-    ''' Returns a flat list with the bandpower values for each channel-band pair, (ch1_delta, ch1_theta,..., ch2_beta)
+    ''' 
+    Returns a flat list for individual epoch with the bandpower values as columns 
+    for each channel-band pair, (ch1_delta, ch1_theta,..., ch2_beta)
     '''
     bands = [(0.5, 4), (4, 8), (8, 13), (13, 30)] # delta, theta, alpha, beta respectively
     features = []
@@ -44,6 +56,9 @@ def extract_frequency_features(epoch_data, sf):
         for band in bands:
             features.append(bandpower(ch_data, sf, band))
     return features
+
+
+
 
 def extract_all_features(epoch_data, sf):
     ''' Extracts both time and frequency-domain features. Features include: first four statistical moments, RMS, number of xero crossings, average bandpower in theta,delta,alpha,beta bands
